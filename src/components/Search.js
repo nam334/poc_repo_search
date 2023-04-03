53e6bdeab8129b627892d90f36871da096b1e607
nam334 <namratadas334@gmail.com>
2023-04-03 20:06:39 +0530

    added filtering logic

--- src/components/Search.js
+++ src/components/Search.js
@@ -2,13 +2,22 @@ import React, { useEffect } from 'react'
 import { PRODUCTS_ENDPOINT } from '../config'
 import { useState } from 'react'
 import { motion } from 'framer-motion'
+import {FaRupeeSign} from "react-icons/fa"
+import {AiFillStar} from "react-icons/ai"
+import { useDispatch, useSelector } from 'react-redux'
+import { addProducts, searchProducts } from '../productSlice'
 const Search = () => {
     const [open, setOpen] = useState(false)
     const [text, setText] = useState(false)
+    const [searchText, setSearchText] = useState('')
+    const dispatch = useDispatch()
+    const filteredProducts = useSelector(store => store.product.filteredProducts)
+    console.log(filteredProducts && filteredProducts)
     useEffect(()=> {
         const fetchData = async () => {
             const data = await fetch(PRODUCTS_ENDPOINT)
             const result  = await data.json()
+            dispatch(addProducts(result))
             console.log(result)
         }
         // const fetchedProducts = fetchData()
@@ -17,7 +26,7 @@ const Search = () => {
     },[])
   return (
     <>
-   <div className='bg-slate-300 h-screen'> 
+   <div className=' h-screen'> 
    <motion.div 
     className='flex  bg-white'
      animate = {{
@@ -28,8 +37,15 @@ const Search = () => {
     text ? <>
     <div className='flex flex-col'>
     <div className='flex items-center p-5 mt-10 '>
-    <input type='text' placeholder='Type what you are looking for...' className='font-mono w-[800px] h-8 placeholder-teal-900
-    focus:outline-none text-2xl font-semibold placeholder-p-4' />
+    <input type='text' placeholder='Type what you are looking for...' 
+     className='font-mono w-[800px] h-8 placeholder-teal-900
+    focus:outline-none text-2xl font-semibold placeholder-p-4' 
+    value={searchText}
+    onChange={(e) => {
+        setSearchText(e.target.value) 
+        dispatch(searchProducts(e.target.value))
+    }}
+    />
     <button type="button" class="rounded-full p-2 inline-flex items-center bg-slate-50
      text-teal-700 hover:text-teal-900 hover:bg-slate-50  focus:outline-none focus:ring-2 
       focus:ring-inset focus:ring-indigo-500" 
@@ -76,7 +92,40 @@ const Search = () => {
   </button>
    }
     </motion.div>
+    <div className='flex flex-wrap '>
+   {
+    filteredProducts.length ? filteredProducts.map(filteredProduct => <>
+   <div className='p-3 flex flex-col hover:bg-slate-200 items-center  w-96 
+    m-4 mx-2 justify-between text-slate-500 fira-sans bg-slate-100 rounded-md'>
+    <div className=' p-4  cursor-pointer  flex justify-center items-center'>
+    <h2 className='text-sm font-semibold text-slate-600 my-2'>{filteredProduct.title}</h2>
+    <img src={filteredProduct.image} alt="" className='w-28 mb-2 h-36 ' /> 
+    </div>
+    <div className="flex items-center justify-between  w-full">  
+    <div className="flex items-center">
+    <FaRupeeSign/>
+    <h4 className='text-sm py-2 text-black-200 font-bold'>
+         {/* {product.price} */}
+         {
+         
+          <span className='strike mr-1'>{filteredProduct.price}</span> 
+       
+         } 
+    </h4>
+    </div>
+    <div className="flex items-center">
+    <h4 className='text-sm py-2 text-black-200 font-semibold'>{filteredProduct?.rating?.rate}</h4>
+    <AiFillStar className='ratingStar' fill="#FF007F" />
+    {/* <Icon fill="#FF007F"/> */}
+  
+    </div>
+    </div> 
+    </div>
+        </>) : <h1>No products</h1>
+   }
    </div>
+   </div>
+  
     </>
   )
 }
--- src/productSlice.js
+++ src/productSlice.js
@@ -3,14 +3,19 @@ import { createSlice } from "@reduxjs/toolkit";
 const productSlice = createSlice({
     name:'product',
     initialState:{
-        products:[]
+        products:[],
+        filteredProducts:[]
     },
     reducers:{
         addProducts:(state, action) => {
             state.products = state.products.concat(action.payload)
+        },
+        searchProducts:(state, action) => {
+            console.log(action.payload)
+            state.filteredProducts = state.products.filter((item) => item.title.toLowerCase().trim().match(action.payload.toLowerCase().trim()))
         }
     }
 })
 
-export const {addProducts} = productSlice.actions 
+export const {addProducts, searchProducts} = productSlice.actions 
 export default productSlice.reducer

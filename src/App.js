506253a74df4644e1b3fc939d6fed857abfcf5cd
nam334 <namratadas334@gmail.com>
2023-04-03 18:06:14 +0530

    setup redux store

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
 import logo from './logo.svg';
 import './App.css';
 import Search from './components/Search';
+import { Provider } from 'react-redux';
+import { store } from './store';
 
 function App() {
   return (
     <>
+    <Provider store={store}>
     <Search/>
+    </Provider>
+    
     </>
   );
 }
